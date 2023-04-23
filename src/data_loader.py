725e1135b2d0dfda76264daa9ce0f99902799882
Vosloo <marek.szydlow@gmail.com>
2023-04-23 22:46:51 +0200

    Added notebook with mne testing; Updated loader and measurement model

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,40 +2,35 @@ from pathlib import Path
 from typing import Generator
 
 import pandas as pd
-
 from src.model.measurement import Measurement
 
-DATA_DIR = Path(__file__).parent.parent / "data"
+DATA_DIR = Path(__file__).parent.parent / "data" / "parquet"
 EVAL_DIR = DATA_DIR / "eval"
 TRAIN_DIR = DATA_DIR / "train"
+EVAL_AGE = DATA_DIR / "eval.csv"
+TRAIN_AGE = DATA_DIR / "train.csv"
 
 
 class DataLoader:
     @staticmethod
     def get_eval(limit: int) -> list[Measurement]:
         batch = []
-        no_files = len(list(EVAL_DIR.iterdir()))
-        print("Loading eval data...")
-        for ind, file in enumerate(EVAL_DIR.iterdir()):
-            print(f"\r{ind + 1:3}/{no_files:3}", end="")
-            with open(file, "r") as f:
-                age_line = f.readline()
+        ages = pd.read_csv(EVAL_AGE, index_col=0)
+        for file in EVAL_DIR.iterdir():
+            age = ages.loc[file.name, "age"]
+            batch.append(Measurement(age, file))
 
-            batch.append(Measurement(age_line, file))
             if len(batch) == limit:
-                print("\nDone")
                 return batch
 
     @staticmethod
     def get_train_iter(batch_size: int) -> Generator[list[Measurement], None, None]:
         batch = []
-        no_files = len(list(TRAIN_DIR.iterdir()))
-        for ind, file in enumerate(TRAIN_DIR.iterdir()):
-            print(f"\r{ind + 1:4}/{no_files:4}", end="")
-            with open(file, "r") as f:
-                age_line = f.readline()
-            
-            batch.append(Measurement(age_line, file))
+        ages = pd.read_csv(TRAIN_AGE, index_col=0)
+        for file in TRAIN_DIR.iterdir():
+            age = ages.loc[file.name, "age"]
+            batch.append(Measurement(age, file))
+
             if len(batch) == batch_size:
                 yield batch
                 batch = []
--- src/model/measurement.py
+++ src/model/measurement.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import pandas as pd
-import regex as re
 
 KEEP_COLUMNS = [
     "EEG FP1-REF",
@@ -35,25 +34,29 @@ KEEP_COLUMNS = [
 
 
 class Measurement:
-    def __init__(self, age_line: str, file: Path) -> None:
-        self.age = self._extract_age(age_line)
+    def __init__(self, age: int, file: Path) -> None:
+        self.age = age
         self.file = file
         self._data = None
 
     @property
     def data(self) -> pd.DataFrame:
         if self._data is None:
-            self._data = pd.read_csv(self.file, skiprows=1)
-            self._data.columns = [item.strip("# ") for item in self._data.columns]
-            self._data = self._data[KEEP_COLUMNS]
+            self._data = pd.read_parquet(self.file, columns=KEEP_COLUMNS)
 
         return self._data
-
-    def _extract_age(self, age_line: str) -> int:
-        return int(re.search(r"\d+", age_line).group(0))
 
     def __repr__(self) -> str:
         return f"Measurement(age={self.age}, file={'/'.join(self.file.parts[-2:])})"
 
     def __str__(self) -> str:
         return self.__repr__()
+
+    def __lt__(self, other: "Measurement") -> bool:
+        return self.age < other.age
+    
+    def __gt__(self, other: "Measurement") -> bool:
+        return self.age > other.age
+    
+    def __eq__(self, other: "Measurement") -> bool:
+        return self.age == other.age
