665f9cb1c11b5583379606fb170e910510934151
Vosloo <marek.szydlow@gmail.com>
2023-04-27 23:52:25 +0200

    Added dataset based on statistics; Updated regressor

--- regressor.py
+++ regressor.py
@@ -10,58 +10,25 @@ from src.model.measurement import Measurement
 MODEL_PATH = Path("xgbRegressor.bin")
 
 
-def get_min_no_rows(measurements: list[Measurement]) -> int:
-    return min([measurement.data.shape[0] for measurement in measurements])
-
-
 if __name__ == "__main__":
     model = XGBRegressor(
-        n_estimators=1000,
-        max_depth=8,
+        n_estimators=200,
+        max_depth=4,
         eta=0.1,
-        n_jobs=5,
+        n_jobs=6,
         verbosity=1,
+        early_stopping_rounds=20,
     )
 
-    eval_list = DataLoader.get_eval(limit=5)
-    min_no_rows = 240752 - 2  # get_min_no_rows(eval_list)
-    X_eval = np.array([measurement.data.values[:min_no_rows].flatten().T for measurement in eval_list])
-    y_eval = np.array([])
-    for ind, measurement in enumerate(eval_list):
-        if ind == min_no_rows:
-            break
+    train_dataset = DataLoader.get_statistic_train()
+    X = train_dataset.drop(columns=["age"])
+    y = train_dataset["age"]
 
-        y_eval = np.append(y_eval, measurement.age)
+    eval_dataset = DataLoader.get_statistic_eval()
+    eval_X = eval_dataset.drop(columns=["age"])
+    eval_y = eval_dataset["age"]
 
-    print(X_eval.shape)
-    print(y_eval.shape)
+    model.fit(X, y, eval_set=[(eval_X, eval_y)])
 
-    evals = [(xgb.DMatrix(X_eval, label=y_eval), "eval")]
-
-    train_iter = DataLoader.get_train_iter(batch_size=10)
-
-    trained_model = model
-    for measurements in train_iter:
-        min_no_rows = 240752 - 2  # get_min_no_rows(measurements)
-        X = np.array(
-            [measurement.data.values[:min_no_rows].flatten().T for measurement in measurements]
-        )
-        y = np.array([])
-        for ind, measurement in enumerate(measurements):
-            if ind == min_no_rows:
-                break
-
-            y = np.append(y, measurement.age)
-
-        print(X.shape)
-        print(y.shape)
-
-        trained_model = xgb.train(
-            params=model.get_xgb_params(),
-            dtrain=xgb.DMatrix(X, label=y),
-            evals=evals,
-            verbose_eval=1,
-            xgb_model=MODEL_PATH if MODEL_PATH.exists() else None,
-        )
-
-        trained_model.save_model(MODEL_PATH)
+    for i, (true, pred) in enumerate(zip(eval_y, model.predict(eval_X))):
+        print(f"True: {true:.2f}, Predicted: {pred:.2f}")
--- src/data_loader.py
+++ src/data_loader.py
@@ -7,21 +7,28 @@ from src.model.measurement import Measurement
 DATA_DIR = Path(__file__).parent.parent / "data" / "parquet"
 EVAL_DIR = DATA_DIR / "eval"
 TRAIN_DIR = DATA_DIR / "train"
-EVAL_AGE = DATA_DIR / "eval.csv"
-TRAIN_AGE = DATA_DIR / "train.csv"
+EVAL_AGE = DATA_DIR / "eval_age.csv"
+TRAIN_AGE = DATA_DIR / "train_age.csv"
 
+TRAIN_STATS_PATH = Path("out/train_stats.csv")
+EVAL_STATS_PATH = Path("out/eval_stats.csv")
 
 class DataLoader:
+    eval_size = len(list(EVAL_DIR.iterdir()))
+    train_size = len(list(TRAIN_DIR.iterdir()))
+
     @staticmethod
-    def get_eval(limit: int) -> list[Measurement]:
+    def get_eval(limit: int = -1) -> list[Measurement]:
         batch = []
         ages = pd.read_csv(EVAL_AGE, index_col=0)
         for file in EVAL_DIR.iterdir():
             age = ages.loc[file.name, "age"]
             batch.append(Measurement(age, file))
 
-            if len(batch) == limit:
+            if limit != -1 and len(batch) == limit:
                 return batch
+        
+        return batch
 
     @staticmethod
     def get_train_iter(batch_size: int) -> Generator[list[Measurement], None, None]:
@@ -34,3 +41,21 @@ class DataLoader:
             if len(batch) == batch_size:
                 yield batch
                 batch = []
+
+        if batch:
+            yield batch
+
+    @staticmethod
+    def get_statistic_train() -> pd.DataFrame:
+        if not TRAIN_STATS_PATH.exists():
+            raise FileNotFoundError("Train statistics not found. Run statistifier.ipynb to generate them.")
+
+        return pd.read_csv(TRAIN_STATS_PATH)
+    
+
+    @staticmethod
+    def get_statistic_eval() -> pd.DataFrame:
+        if not EVAL_STATS_PATH.exists():
+            raise FileNotFoundError("Eval statistics not found. Run statistifier.ipynb to generate them.")
+
+        return pd.read_csv(EVAL_STATS_PATH)
