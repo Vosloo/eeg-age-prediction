139ca2cc4c586d4a85726581601d280dfcef3d93
Vosloo <marek.szydlow@gmail.com>
2023-05-11 18:34:53 +0200

    Added FFT

--- regressor.py
+++ regressor.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 import numpy as np
-import xgboost as xgb
+import pandas as pd
 from xgboost import XGBRegressor
+from sklearn.linear_model import LinearRegression
 
 from src.data_loader import DataLoader
 from src.model.measurement import Measurement
@@ -11,24 +12,27 @@ MODEL_PATH = Path("xgbRegressor.bin")
 
 
 if __name__ == "__main__":
-    model = XGBRegressor(
-        n_estimators=200,
-        max_depth=4,
-        eta=0.1,
-        n_jobs=6,
-        verbosity=1,
-        early_stopping_rounds=20,
-    )
+    # model = XGBRegressor(
+    #     n_estimators=200,
+    #     max_depth=4,
+    #     eta=0.1,
+    #     n_jobs=6,
+    #     verbosity=1,
+    #     early_stopping_rounds=20,
+    # )
+    model = LinearRegression(n_jobs=6)
 
-    train_dataset = DataLoader.get_statistic_train()
-    X = train_dataset.drop(columns=["age"])
-    y = train_dataset["age"]
+    # train_dataset = DataLoader.get_statistic_train()
+    train_dataset = pd.read_csv("out/fft_train.csv")
+    X = train_dataset.iloc[:, :-1]
+    y = train_dataset.iloc[:, -1]
 
-    eval_dataset = DataLoader.get_statistic_eval()
-    eval_X = eval_dataset.drop(columns=["age"])
-    eval_y = eval_dataset["age"]
+    eval_dataset = pd.read_csv("out/fft_eval.csv")
+    eval_X = eval_dataset.iloc[:, :-1]
+    eval_y = eval_dataset.iloc[:, -1]
 
-    model.fit(X, y, eval_set=[(eval_X, eval_y)])
+    # model.fit(X, y, eval_set=[(eval_X, eval_y)])
+    model.fit(X, y)
 
     for i, (true, pred) in enumerate(zip(eval_y, model.predict(eval_X))):
         print(f"True: {true:.2f}, Predicted: {pred:.2f}")
