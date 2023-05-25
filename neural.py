a23d7da54f57779672ef6378c5a2a95ad796c60c
Vosloo <marek.szydlow@gmail.com>
2023-05-25 22:49:56 +0200

    Added results for neural net; Updated fft and regressor

--- neural.py
+++ neural.py
@@ -1,53 +1,198 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import torch
-import pandas as pd
+import torch.nn.functional as F
 from torch import nn
+from torch.utils.data import DataLoader, Dataset
+
+sns.set_theme()
+
+class EEGDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
 
 
-class AgePrediction(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
-        super(AgePrediction, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, 1)
+class LSTM(nn.Module):
+    def __init__(self):
+        super(LSTM, self).__init__()
+
+        self.lstm = nn.LSTM(input_size=21, hidden_size=64, num_layers=2, batch_first=True, dropout=0.2)
+        self.fc = nn.Linear(64, 1)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        lstm_out, _ = self.lstm(x)
+        # Take the output from the final timetep
+        final_time_step_out = lstm_out[:, -1, :]
+        y_pred = self.fc(final_time_step_out)
+        return y_pred
 
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
-        return out
-    
+
+class ConvLSTM1(nn.Module):
+    def __init__(self):
+        super(ConvLSTM1, self).__init__()
+        self.conv1 = nn.Conv1d(21, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm1d(32)
+        self.lstm = nn.LSTM(input_size=32, hidden_size=64, num_layers=2, batch_first=True, dropout=0.5)
+        self.fc1 = nn.Linear(64, 32)
+        self.bn2 = nn.BatchNorm1d(32)
+        self.fc2 = nn.Linear(32, 1)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)  # rearrange to (batch_size, num_channels, sequence_length)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = x.permute(0, 2, 1)  # rearrange back to (batch_size, sequence_length, num_channels)
+        lstm_out, _ = self.lstm(x)
+        final_time_step_out = lstm_out[:, -1, :]  # take the output from the final timestep
+        x = F.relu(self.bn2(self.fc1(final_time_step_out)))  # ReLU activation and batch normalization
+        y_pred = self.fc2(x)
+        return y_pred
+
+
+class ConvLSTM(nn.Module):
+    def __init__(self):
+        super(ConvLSTM, self).__init__()
+        self.conv1 = nn.Conv1d(21, 32, kernel_size=3, stride=1, padding=1)
+        self.lstm = nn.LSTM(input_size=32, hidden_size=64, num_layers=2, batch_first=True, dropout=0.5)
+        self.fc = nn.Linear(64, 1)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)  # rearrange to (batch_size, num_channels, sequence_length)
+        x = F.relu(self.conv1(x))
+        x = x.permute(0, 2, 1)  # rearrange back to (batch_size, sequence_length, num_channels)
+        lstm_out, _ = self.lstm(x)
+        final_time_step_out = lstm_out[:, -1, :]  # take the output from the final timestep
+        y_pred = self.fc(final_time_step_out)
+        return y_pred
+
 
 def get_data() -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    train_dataset = pd.read_csv("out/fft_train.csv").to_numpy()  # 1171x1000x22
-    eval_dataset = pd.read_csv("out/fft_eval.csv").to_numpy()  # 126x1000x22
+    train_data = np.load("out/fft_train_full_1000.npy")
+    eval_data = np.load("out/fft_eval_full_1000.npy")
 
-    train_data = train_dataset[:, :, :-1]
-    train_labels = train_dataset[:, :, -1]
-    eval_data = eval_dataset[:, :, :-1]
-    eval_labels = eval_dataset[:, :, -1]
+    train_age = np.load("out/fft_train_ages_1000.npy")
+    eval_age = np.load("out/fft_eval_ages_1000.npy")
 
-    train_data = torch.tensor(train_data, dtype=torch.float32)
-    train_labels = torch.tensor(train_labels, dtype=torch.float32)
-    eval_data = torch.tensor(eval_data, dtype=torch.float32)
-    eval_labels = torch.tensor(eval_labels, dtype=torch.float32)
+    train_data = torch.from_numpy(train_data)
+    train_labels = torch.from_numpy(train_age)
+    eval_data = torch.from_numpy(eval_data)
+    eval_labels = torch.from_numpy(eval_age)
 
-    return train_data, train_labels, eval_data, eval_labels
+    # Standardize data
+    train_mean = train_data.mean()
+    train_std = train_data.std()
+    train_data = (train_data - train_mean) / train_std
+    eval_data = (eval_data - train_mean) / train_std
+
+    train_dataset = EEGDataset(train_data, train_labels.view(-1, 1))
+    eval_dataset = EEGDataset(eval_data, eval_labels.view(-1, 1))
+
+    train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+    eval_dataloader = DataLoader(eval_dataset, batch_size=128)
+
+    return train_dataloader, eval_dataloader
+
+
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv1d:
+        nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
 
 
 if __name__ == '__main__':
-    train_data, train_labels, eval_data, eval_labels = get_data()
-    model = AgePrediction(input_size=21, hidden_size=150, num_layers=2)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_dataloader, eval_dataloader = get_data()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    model = LSTM()
+    model.apply(init_weights)
     model = model.to(device)
 
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)  # learning rate scheduler
 
-    num_epochs = 10
-    for epoch in range(num_epochs):
+    epochs = 200
+    best_loss = float("inf")
+    for epoch in range(epochs):
         model.train()
-        
+        for i, (train_data, train_labels) in enumerate(train_dataloader):
+            train_data = train_data.to(device, dtype=torch.float)
+            train_labels = train_labels.to(device, dtype=torch.float)
 
+            optimizer.zero_grad()
+
+            outputs = model(train_data)
+            loss = torch.sqrt(criterion(outputs, train_labels))
+
+            loss.backward()
+            optimizer.step()
+
+        scheduler.step()
+
+        model.eval()
+        with torch.no_grad():
+            eval_losses = []
+            for i, (inputs, labels) in enumerate(eval_dataloader):
+                inputs = inputs.to(device, dtype=torch.float)
+                labels = labels.to(device, dtype=torch.float)
+
+                outputs = model(inputs)
+                loss = torch.sqrt(criterion(outputs, labels))
+                eval_losses.append(loss.item())
+            avg_eval_loss = sum(eval_losses) / len(eval_losses)
+
+        print(f"Epoch {epoch+1}/{epochs} - Loss: {loss.item():.4f} - Eval loss: {avg_eval_loss:.4f}")
+
+        # Save the model if the evaluation loss decreased.
+        if avg_eval_loss < best_loss:
+            print(f"New best model found! Eval loss: {avg_eval_loss:.4f}")
+            best_loss = avg_eval_loss
+            torch.save(model.state_dict(), 'best_model.pth')
+
+    # Show predictions
+    model.load_state_dict(torch.load('best_model.pth'))
+    model.eval()
+    with torch.no_grad():
+        predictions = []
+        for i, (inputs, labels) in enumerate(eval_dataloader):
+            inputs = inputs.to(device, dtype=torch.float)
+            labels = labels.to(device, dtype=torch.float)
+
+            outputs = model(inputs)
+            out = outputs.cpu().numpy()
+            predictions.append(out)
+        predictions = np.concatenate(predictions)
+
+    for i, (pred, label) in enumerate(zip(predictions, eval_dataloader.dataset.labels)):
+        print(f"Actual: {label[0]:.2f} - Predicted: {pred[0]:.2f}")
+
+"""
+Example output from predictions - predicted age is basically the mean of the training data :(
+--------------------------------
+Actual: 65.00 - Predicted: 44.05
+Actual: 79.00 - Predicted: 44.05
+Actual: 56.00 - Predicted: 44.05
+Actual: 58.00 - Predicted: 44.05
+Actual: 31.00 - Predicted: 44.05
+Actual: 48.00 - Predicted: 44.05
+Actual: 58.00 - Predicted: 44.05
+Actual: 43.00 - Predicted: 44.05
+Actual: 61.00 - Predicted: 44.05
+Actual: 29.00 - Predicted: 44.05
+Actual: 48.00 - Predicted: 44.05
+Actual: 71.00 - Predicted: 44.05
+Actual: 62.00 - Predicted: 44.05
+Actual: 43.00 - Predicted: 44.04
+Actual: 36.00 - Predicted: 44.05
+Actual: 36.00 - Predicted: 44.04
+Actual: 55.00 - Predicted: 44.03
+Actual: 52.00 - Predicted: 44.04
+Actual: 81.00 - Predicted: 44.05
+"""
--- regressor.py
+++ regressor.py
@@ -3,21 +3,12 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from joblib import dump, load
-from sklearn.decomposition import FastICA
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 from xgboost import XGBRegressor
 
-from src.data_loader import DataLoader
-from src.model.measurement import Measurement
-
-N_COMPONENTS = 600
-FUN = "cube"
-
 MODEL_PATH = Path("xgbRegressor.bin")
 SCALER_PATH = Path("out/StandardScaler.joblib")
-ICA_PATH = Path(f"out/ICA_{FUN}_{N_COMPONENTS}.joblib")
 
 
 def standardize_data(data: np.array) -> np.array:
@@ -25,42 +16,31 @@ def standardize_data(data: np.array) -> np.array:
     if not SCALER_PATH.exists():
         scaler = StandardScaler()
         scaler.fit(data)
-        dump(scaler, SCALER_PATH)
+        # dump(scaler, SCALER_PATH)
     else:
         scaler = load(SCALER_PATH)
 
     return scaler.transform(data)
 
 
-def ica_data(data: np.array) -> np.array:
-    print("ICA data...")
-    if not ICA_PATH.exists():
-        ica = FastICA(n_components=N_COMPONENTS, fun=FUN, tol=1e-3, max_iter=5000, whiten='unit-variance')
-        ica.fit(data)
-        dump(ica, ICA_PATH)
-    else:
-        ica = load(ICA_PATH)
-
-    return ica.transform(data)
-
 def train_regressor():
     print("Loading data...")
     model = XGBRegressor(
         n_estimators=400,
-        max_depth=5,
+        max_depth=6,
         eta=0.02,
         n_jobs=6,
         verbosity=1,
         early_stopping_rounds=100,
     )
-    train_dataset = pd.read_csv("out/fft_train_full.csv")
-    print("Pre ICA shape:", train_dataset.iloc[:, :-1].shape)
-    X = ica_data(standardize_data(train_dataset.iloc[:, :-1].values))
-    print("After ICA shape:", X.shape)
+    train_dataset = pd.read_csv("out/fft_train.csv")
+    X = standardize_data(train_dataset.iloc[:, :-1].values)
+    # X = train_dataset.iloc[:, :-1].values
     y = train_dataset.iloc[:, -1]
 
-    eval_dataset = pd.read_csv("out/fft_eval_full.csv")
-    eval_X = ica_data(standardize_data(eval_dataset.iloc[:, :-1].values))
+    eval_dataset = pd.read_csv("out/fft_eval.csv")
+    eval_X = standardize_data(eval_dataset.iloc[:, :-1].values)
+    # eval_X = eval_dataset.iloc[:, :-1].values
     eval_y = eval_dataset.iloc[:, -1]
 
     model.fit(X, y, eval_set=[(eval_X, eval_y)])
@@ -76,13 +56,13 @@ def train_svr():
         verbose=True,
     )
     train_dataset = pd.read_csv("out/fft_train.csv")
-    print("Pre ICA shape:", train_dataset.iloc[:, :-1].shape)
-    X = ica_data(standardize_data(train_dataset.iloc[:, :-1].values))
-    print("After ICA shape:", X.shape)
+    X = standardize_data(train_dataset.iloc[:, :-1].values)
+    # X = train_dataset.iloc[:, :-1].values
     y = train_dataset.iloc[:, -1]
 
     eval_dataset = pd.read_csv("out/fft_eval.csv")
-    eval_X = ica_data(standardize_data(eval_dataset.iloc[:, :-1].values))
+    eval_X = standardize_data(eval_dataset.iloc[:, :-1].values)
+    # eval_X = eval_dataset.iloc[:, :-1].values
     eval_y = eval_dataset.iloc[:, -1]
 
     model.fit(X, y)
